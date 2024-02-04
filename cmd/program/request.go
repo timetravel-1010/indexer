ebd9962179c4c12a30986dad0495d82598be8ad2
timetravel-1010 <timetravel_1010@protonmail.com>
2024-02-03 22:24:01 -0500

    Add bulk ingestion implementation

--- cmd/program/request.go
+++ cmd/program/request.go
@@ -34,18 +34,24 @@ type Payload struct {
 	DocumentData []Document `json:"records"`
 }
 
+var client = &http.Client{}
+
 // Upload
 func Upload(re HttpRequest, payload *bytes.Buffer) {
-	u := fmt.Sprintf("http://%s:%s/api/_bulkv2/", re.BaseURL, re.Port) //, re.Index, re.Type)
+	//u := fmt.Sprintf("http://%s:%s/api/_bulkv2/", re.BaseURL, re.Port) //, re.Index, re.Type)
+
+	u := fmt.Sprintf("http://%s:%s/api/_bulk", re.BaseURL, re.Port) //, re.Index, re.Type)
 	req, err := http.NewRequest("POST", u, payload)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 	req.SetBasicAuth(re.Creds.User, re.Creds.Password)
-	req.Header.Set("Content-Type", "application/json")
+	//req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Content-Type", "application/x-ndjson")
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36")
 
-	resp, err := http.DefaultClient.Do(req)
+	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -57,4 +63,5 @@ func Upload(re HttpRequest, payload *bytes.Buffer) {
 		log.Fatal(err)
 	}
 	log.Println(string(body))
+	payload.Reset()
 }
