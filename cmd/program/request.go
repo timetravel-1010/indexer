8dc02ec184ab09189825ce8cb04880e8131b1045
timetravel-1010 <timetravel_1010@protonmail.com>
2024-02-05 19:34:21 -0500

    Add return error to the Upload function

--- cmd/program/request.go
+++ cmd/program/request.go
@@ -2,6 +2,7 @@ package program
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -37,31 +38,44 @@ type Payload struct {
 var client = &http.Client{}
 
 // Upload
-func Upload(re HttpRequest, payload *bytes.Buffer) {
+func Upload(re HttpRequest, payload *bytes.Buffer) error {
 	//u := fmt.Sprintf("http://%s:%s/api/_bulkv2/", re.BaseURL, re.Port) //, re.Index, re.Type)
 
 	u := fmt.Sprintf("http://%s:%s/api/_bulk", re.BaseURL, re.Port) //, re.Index, re.Type)
 	req, err := http.NewRequest("POST", u, payload)
-
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
+
 	req.SetBasicAuth(re.Creds.User, re.Creds.Password)
 	//req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Content-Type", "application/x-ndjson")
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36")
+	req.Header.Set("User-Agent", "laptop")
 
 	resp, err := client.Do(req)
+	body, err := getBodyResponse(resp)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	defer resp.Body.Close()
 
 	log.Println(resp.StatusCode)
+	log.Println(body)
+	if resp.StatusCode == http.StatusInternalServerError {
+		return errors.New("internal server error")
+	}
+
+	payload.Reset()
+
+	return nil
+}
+
+func getBodyResponse(resp *http.Response) (string, error) {
+
+	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
-	log.Println(string(body))
-	payload.Reset()
+
+	return string(body), nil
 }
