061cfb3a590941fd7e284c188a4a26e7941d51f2
timetravel-1010 <timetravel_1010@protonmail.com>
2024-02-11 22:08:11 -0500

    Add move Email stuff to email.go

--- cmd/program/parser.go
+++ cmd/program/parser.go
@@ -5,101 +5,30 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"strings"
-
-	"github.com/timetravel-1010/indexer/cmd/util"
 )
 
 var (
 	fields = []string{
-		"Message-ID",
-		"Date",
-		"From",
-		"To",
-		"Cc",
-		"Bcc",
-		"Subject",
-		"Mime-Version",
-		"Content-Type",
-		"Content-Transfer-Encoding",
-		"X-From",
-		"X-To",
-		"X-cc",
-		"X-bcc",
-		"X-Folder",
-		"X-Origin",
-		"X-FileName",
+		"Message-ID: ",
+		"Date: ",
+		"From: ",
+		"To: ",
+		"Cc: ",
+		"Bcc: ",
+		"Subject: ",
+		"Mime-Version: ",
+		"Content-Type: ",
+		"Content-Transfer-Encoding: ",
+		"X-From: ",
+		"X-To: ",
+		"X-cc: ",
+		"X-bcc: ",
+		"X-Folder: ",
+		"X-Origin: ",
+		"X-FileName: ",
 		"Body",
 	}
 )
-
-// An Email contains all the information of an e-mail.
-type Email struct {
-	MessageID               string   `json:"messageId"`
-	Date                    string   `json:"date"`
-	From                    string   `json:"from"`
-	To                      []string `json:"to"`
-	CC                      []string `json:"cc"`
-	BCC                     []string `json:"bcc"`
-	Subject                 string   `json:"subject"`
-	MimeVersion             string   `json:"mimeVersion"`
-	ContentType             string   `json:"contentType"`
-	ContentTransferEncoding string   `json:"contentTransferEncoding"`
-	XFrom                   string   `json:"xFrom"`
-	XTo                     []string `json:"xTo"`
-	Xcc                     []string `json:"xcc"`
-	Xbcc                    []string `json:"xbcc"`
-	XFolder                 string   `json:"xFolder"`
-	XOrigin                 string   `json:"xOrigin"`
-	XFileName               string   `json:"xFileName"`
-	Body                    string   `json:"body"`
-}
-
-// An EmailBuilder
-type EmailBuilder struct {
-	MessageID               strings.Builder
-	Date                    strings.Builder
-	From                    strings.Builder
-	To                      []string
-	CC                      []string
-	BCC                     []string
-	Subject                 strings.Builder
-	MimeVersion             strings.Builder
-	ContentType             strings.Builder
-	ContentTransferEncoding strings.Builder
-	XFrom                   strings.Builder
-	XTo                     []string
-	Xcc                     []string
-	Xbcc                    []string
-	XFolder                 strings.Builder
-	XOrigin                 strings.Builder
-	XFileName               strings.Builder
-	Body                    strings.Builder
-}
-
-// build
-func (eb *EmailBuilder) build() *Email {
-	return &Email{
-		MessageID:               eb.MessageID.String(),
-		Date:                    eb.Date.String(),
-		From:                    eb.From.String(),
-		To:                      eb.To,
-		CC:                      eb.CC,
-		BCC:                     eb.BCC,
-		Subject:                 eb.Subject.String(),
-		MimeVersion:             eb.MimeVersion.String(),
-		ContentType:             eb.ContentType.String(),
-		ContentTransferEncoding: eb.ContentTransferEncoding.String(),
-		XFrom:                   eb.XFrom.String(),
-		XTo:                     eb.XTo,
-		Xcc:                     eb.Xcc,
-		Xbcc:                    eb.Xbcc,
-		XFolder:                 eb.XFolder.String(),
-		XOrigin:                 eb.XOrigin.String(),
-		XFileName:               eb.XFileName.String(),
-		Body:                    eb.Body.String(),
-	}
-}
 
 // A Document contains the path of the email and the email itself.
 type Document struct {
@@ -113,8 +42,7 @@ type Parser struct{}
 // Parse parses the txt email file into the Email structure.
 // If there is an error, it will be of type *PathError.
 func (p *Parser) Parse(filePath string) (*Email, error) {
-	//em := Email{}
-	var eb EmailBuilder = EmailBuilder{}
+	eb := NewEmailBuilder()
 
 	file, err := os.Open(filePath)
 	if err != nil {
@@ -123,13 +51,11 @@ func (p *Parser) Parse(filePath string) (*Email, error) {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	currentField := ""
-	inBody := false
 
 	// Check if the file is an email
 	scanner.Scan()
 	line := scanner.Text()
-	saveLine(&eb, &line, &currentField, filePath, &inBody)
+	eb.SaveLine(&line, filePath)
 	isEmail := eb.MessageID.Len() != 0
 	if !isEmail {
 		return nil, errors.New(fmt.Sprintf("the file %s is not an email", filePath))
@@ -137,7 +63,7 @@ func (p *Parser) Parse(filePath string) (*Email, error) {
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		saveLine(&eb, &line, &currentField, filePath, &inBody)
+		eb.SaveLine(&line, filePath)
 	}
 	return eb.build(), nil
 }
@@ -159,148 +85,4 @@ func MapStrings(arr []string, f func(string) string) []string {
 		newArr[i] = f(s)
 	}
 	return newArr
-}
-
-// saveLine
-func saveLine(em *EmailBuilder, line *string, currentField *string, filePath string, inBody *bool) {
-
-	if *inBody {
-		em.Body.WriteString("\n")
-		em.Body.WriteString(*line)
-		return
-	}
-	// Pending try to do this using strings.Prefix
-	//subStrings := strings.SplitN(*line, ":", 2)
-	for _, field := range fields {
-		// New field
-		if strings.HasPrefix(*line, field) {
-			strings.Pre
-			setValue(*currentField, line, em, filePath)
-		}
-		// Continues in a field
-		addLine(line, *currentField, em, filePath)
-	}
-
-	// Check if continues in a section.
-	// Probably this will not work always.
-	if idx := util.IndexOf(subStrings[0], fields); idx == -1 && em.Body.Len() == 0 {
-		addLine(line, *currentField, em, filePath)
-	} else if len(subStrings) == 2 {
-		*currentField = subStrings[0]
-		line := strings.TrimSpace(subStrings[1])
-		setValue(*currentField, &line, em, filePath)
-	}
-	*inBody = *currentField == "X-FileName"
-}
-
-// setValue
-func setValue(currentField string, l *string, em *EmailBuilder, filePath string) {
-	switch currentField {
-	case "Message-ID":
-		em.MessageID.WriteString(*l)
-	case "Date":
-		em.Date.WriteString(*l)
-	case "From":
-		em.From.WriteString(*l)
-	case "To":
-		em.To = parseAddresses(*l)
-		em.To = append(em.To, parseNames(*l)...)
-	case "Cc":
-		em.CC = parseAddresses(*l)
-		em.CC = append(em.CC, parseNames(*l)...)
-	case "Bcc":
-		em.BCC = parseAddresses(*l)
-		em.BCC = append(em.BCC, parseNames(*l)...)
-	case "Subject":
-		em.Subject.WriteString(*l)
-	case "Mime-Version":
-		em.MimeVersion.WriteString(*l)
-	case "Content-Type":
-		em.ContentType.WriteString(*l)
-	case "Content-Transfer-Encoding":
-		em.ContentTransferEncoding.WriteString(*l)
-	case "X-From":
-		em.XFrom.WriteString(*l)
-	case "X-To":
-		em.XTo = MapStrings(strings.Split(*l, ","), strings.TrimSpace)
-	case "X-cc":
-		em.Xcc = parseAddresses(*l)
-		em.Xcc = append(em.Xcc, parseNames(*l)...)
-	case "X-bcc":
-		em.Xbcc = parseAddresses(*l)
-		em.Xbcc = append(em.Xbcc, parseNames(*l)...)
-	case "X-Folder":
-		em.XFolder.WriteString(*l)
-	case "X-Origin":
-		em.XOrigin.WriteString(*l)
-	case "X-FileName":
-		em.XFileName.WriteString(*l)
-	default:
-		fmt.Println(fmt.Sprintf(`
-        ===================ERROR NO MATCH FOUND
-        function: setValue
-        l: %s
-        currentLine: %s
-        file: %s
-        ===================END ERROR`, *l, currentField, filePath))
-	}
-}
-
-// addLine
-func addLine(l *string, currentField string, em *EmailBuilder, filePath string) {
-
-	switch currentField {
-	case "Message-ID":
-		em.MessageID.WriteString("\n")
-		em.MessageID.WriteString(*l)
-	case "Date":
-		em.Date.WriteString("\n")
-		em.Date.WriteString(*l)
-	case "From":
-		em.From.WriteString("\n")
-		em.From.WriteString(*l)
-	case "To":
-		em.To = append(em.To, parseAddresses(*l)...)
-		em.To = append(em.To, parseNames(*l)...)
-	case "Cc":
-		em.CC = append(em.CC, parseAddresses(*l)...)
-		em.CC = append(em.CC, parseNames(*l)...)
-	case "Bcc":
-		em.BCC = append(em.BCC, parseAddresses(*l)...)
-		em.BCC = append(em.BCC, parseNames(*l)...)
-	case "Subject":
-		em.Subject.WriteString("\n")
-		em.Subject.WriteString(*l)
-	case "Mime-Version":
-		em.MimeVersion.WriteString("\n")
-		em.MimeVersion.WriteString(*l)
-	case "Content-Type":
-		em.ContentType.WriteString("\n")
-		em.ContentType.WriteString(*l)
-	case "Content-Transfer-Encoding":
-		em.ContentTransferEncoding.WriteString("\n")
-		em.ContentTransferEncoding.WriteString(*l)
-	case "X-From":
-		em.XFrom.WriteString("\n")
-		em.XFrom.WriteString(*l)
-	case "X-To":
-		em.XTo = append(em.XTo, MapStrings(strings.Split(*l, ","), strings.TrimSpace)...)
-	case "X-cc":
-		em.Xcc = append(em.Xcc, parseAddresses(*l)...)
-		em.Xcc = append(em.Xcc, parseNames(*l)...)
-	case "X-bcc":
-		em.Xbcc = append(em.Xbcc, parseAddresses(*l)...)
-		em.Xbcc = append(em.Xbcc, parseNames(*l)...)
-	case "X-Folder":
-		em.XFolder.WriteString("\n")
-		em.XFolder.WriteString(*l)
-	case "X-Origin":
-		em.XOrigin.WriteString("\n")
-		em.XOrigin.WriteString(*l)
-	case "X-FileName":
-		em.XFileName.WriteString("\n")
-		em.XFileName.WriteString(*l)
-	default:
-		fmt.Println("addLine: No match found and currentLine =", currentField, "file:", filePath)
-	}
 }
